a04568e54c85057ae94a7666796b18bd53fa23bb
qwab17 <quadriwasiubiodun@gmail.com>
2023-11-16 17:34:45 -0800

    push pall

--- push_pall.c
+++ push_pall.c
@@ -44,3 +44,23 @@ void pall(stack_t **stack, unsigned int line_number)
 		current = current->next;
 	}
 }
+
+
+
+#include "monty.h"
+
+/**
+ * pint - Prints the value at the top of the stack.
+ * @stack: Double pointer to the beginning of the stack.
+ * @line_number: The line number in the Monty bytecodes file.
+ */
+void pint(stack_t **stack, unsigned int line_number)
+{
+	if (stack == NULL || *stack == NULL)
+	{
+		fprintf(stderr, "L%u: can't pint, stack empty\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+
+	printf("%d\n", (*stack)->n);
+}
