d87df11eed5cf320d4bfa5abacbc8f6b6efa738d
qwab17 <quadriwasiubiodun@gmail.com>
2023-11-16 16:49:59 -0800

    header file

---
+++ monty.h
@@ -0,0 +1,44 @@
+#ifndef MONTY_H
+#define MONTY_H
+
+#define _GNU_SOURCE
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <stdarg.h>
+
+/**
+ * struct stack_s - doubly linked list representation of a stack (or queue)
+ * @n: integer
+ * @prev: points to the previous element of the stack (or queue)
+ * @next: points to the next element of the stack (or queue)
+ */
+
+typedef struct stack_s
+{
+    int n;
+    struct stack_s *prev;
+    struct stack_s *next;
+} stack_t;
+
+/**
+ * get_op_func - Selects the correct function to perform an operation
+ * @opcode: The operation to perform
+ * Return: A pointer to the function that corresponds to the opcode
+ */
+
+typedef struct instruction_s
+{
+    char *opcode;
+    void (*f)(stack_t **stack, unsigned int line_number);
+} instruction_t;
+
+int is_number(const char *str);
+void push_stack(stack_t **stack, int value);
+void push(stack_t **stack, unsigned int line_number);
+void pall(stack_t **stack, unsigned int line_number);
+
+
+#endif
